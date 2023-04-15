cecc0692cc10124036c5638c501b525fb01bb35b
Pedro Vitor Emanuel <ped.frsouza@gmail.com>
2023-04-14 22:07:10 -0300

    Aula 2: (Obstáculos)

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,19 +1,20 @@
 import pygame
 
+from pygame.sprite import Sprite
 from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
+
 
 X_POS = 80
 Y_POS = 310
 JUMP_VEL = 8.5
 Y_POS_DUCK = 340
 
-class Dinosaur:
+class Dinosaur(Sprite):
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS
-        
         self.step_index = 0
         self.jump_vel = JUMP_VEL
         self.dino_jump = False
@@ -28,23 +29,19 @@ class Dinosaur:
         elif self.dino_jump:
             self.jump()
 
-
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
             self.dino_duck = False
-
         elif user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_jump = False
             self.dino_run = False
             self.dino_duck = True
-
-
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_run = True
             self.dino_duck = False
-
+            
         if self.step_index >= 10:
             self.step_index = 0
 
@@ -67,13 +64,10 @@ class Dinosaur:
         if self.dino_jump:
             self.dino_rect.y -= self.jump_vel * 4
             self.jump_vel -= 0.8
-
         if self.jump_vel < -JUMP_VEL:
             self.dino_rect.y = Y_POS
             self.dino_jump = False
             self.jump_vel = JUMP_VEL
-
-
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
